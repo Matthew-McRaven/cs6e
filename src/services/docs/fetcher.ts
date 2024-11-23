4857230f58bca287d717a63dedb0638823bd8285
David Nicholas <dnicholas@bitovi.com>
2024-11-22 22:35:05 -0800

    d loading

---
+++ src/services/docs/fetcher.ts
@@ -0,0 +1,33 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+
+interface FileResponse {
+  name: string;
+  path: string;
+}
+
+export const getSections = async (): Promise<FileResponse[]> => {
+  const directoryContent = await fs.readdir(path.resolve("./src/services/docs/content"), {
+    withFileTypes: true,
+  });
+
+  return directoryContent
+    .filter((dirent) => dirent.isDirectory)
+    .map((dirent) => ({ name: dirent.name, path: dirent.parentPath + "/" + dirent.name }));
+};
+
+export const getFilesFor = async (section: Pick<FileResponse, "path">): Promise<FileResponse[]> => {
+  const sectionFiles = await fs.readdir(section.path);
+
+  return sectionFiles
+    .filter((file) => file.includes(".md"))
+    .map((file) => ({
+      name: file.split("/").at(-1)?.replace(".md", "") ?? "",
+      path: section.path + "/" + file,
+    }));
+};
+
+export const getFileContent = async (file: Pick<FileResponse, "path">): Promise<string> => {
+  console.log({ file });
+  return fs.readFile(file.path, "utf-8");
+};
