4df0bc18b43bbc9f2913041839344fc8b07c4cf4
David Nicholas <dnicholas@bitovi.com>
2024-12-06 13:54:55 -0600

    ide

---
+++ src/app/ide/components/ide/ide.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import type { FC } from "react";
+
+const Ide: FC = () => {
+  return <div>Matt do things here</div>;
+};
+
+export default Ide;
---
+++ src/app/ide/page.tsx
@@ -0,0 +1,10 @@
+import SiteHeader from "../components/site-header";
+
+const Page = async () => {
+  return (
+    <>
+      <SiteHeader />
+      <Ide />
+    </>
+  );
+};
