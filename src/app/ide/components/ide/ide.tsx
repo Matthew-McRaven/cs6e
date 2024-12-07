1deb5fdc6df0acdff30fe401c6f350025e198e02
Matthew McRaven <matthew.mcraven@gmail.com>
2024-12-07 14:59:18 -0800

    Add WASM application guts
    Sorry David, I had to modify the window object to patch fetch for the WASM blob.
    I'm sure there's a better way, but it's what I could get working.

--- src/app/ide/components/ide/ide.tsx
+++ src/app/ide/components/ide/ide.tsx
@@ -1,9 +1,69 @@
 "use client";
 
-import type { FC } from "react";
+import {FC, useState} from "react";
+import {useEffect}  from "react";
+import {qtLoad} from "./qtloader";
 
 const Ide: FC = () => {
-  return <div>Matt do things here</div>;
+  const [loaded, setLoaded] = useState<boolean>(false)
+  const [errorText, setErrorText] = useState("")
+  // Hack to intercept the fetch request to the WASM blob.
+  useEffect(() => {
+    const asyncInner = async () => {
+      const {fetch: origFetch} = window;
+      window.fetch = async (...args) => {
+        // If trying to fetch
+        if (String(args[0]).match(/wasm$/i)) return await origFetch("https://compsys-pep.com/" + args[0], ...args.slice(1))
+        else return await origFetch(...args);
+      };
+    };
+    asyncInner().catch(console.log)
+  }, []);
+
+  // Start the Qt application and handle its lifecycle events.
+  useEffect(()=> {
+    const asyncInner = async () => {
+      const screen = document.querySelector("#screen")
+      try {
+        await qtLoad({
+          qt: {
+            onLoaded: () => setLoaded(true),
+            onExit: (exitData: { code: number, text?: string }) => {
+              const lines = [
+                `Application exit with code ${exitData.code}`,
+                exitData.text ? ` (${exitData.text})` : ''
+              ]
+              setErrorText(lines.join("\n"))
+              setLoaded(false)
+            },
+            // @ts-expect-error, injected into window by qtLoad.
+            entryFunction: window.pepp_entry,
+            containerElements: [screen],
+          }
+        });
+      } catch (e) {
+        console.error(e);
+        // @ts-expect-error, injected by WASM runtime
+        console.error(e.stack);
+      }
+    }
+    asyncInner().catch(console.log)
+  },[])
+
+
+  return <div>
+    {/*Needs to be sync, otherwise my useEffect to do the load explodes.*/}
+    <script src="https://compsys-pep.com/pepp.js"/>
+    <figure id="qtspinner" style={{"display":loaded ? "none" :"block"}}>
+      <center>
+        <img src="https://compsys-pep.com/qtlogo.svg" width="320" height="200"/>
+        <strong>Loading Pepp IDE</strong>
+        <div id="qtstatus">{errorText}</div>
+        <noscript>JavaScript is disabled. Please enable JavaScript to use this application.</noscript>
+      </center>
+    </figure>
+    <div id="screen"  style={{"display":loaded ? "blocK" : "none", "width":"100%", "height":"100vh"}}/>
+  </div>;
 };
 
 export default Ide;
